6791e13ef25ebad4b692d7144d53269fd9b1715f
João Victor <124260831+JoaoVictxrz@users.noreply.github.com>
2023-09-25 14:58:39 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let numeros = [1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19, 20]
+const maior = document.getElementById("maior");
+const menor = document.getElementById("menor");
+const selectElement = document.getElementById('select');
+const pElement = document.getElementById('p');
+// mostrar o número da array
+function gerar() {
+    const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+    document.getElementById("nmr"); nmr.innerHTML = numeroAleatorio;
+    function remove(){
+       
+    }
+}
+
+// Pegar o select maior ou menor
+// Verifcar se é maior ou menor
+
+// conferir se continua ou para
+
+// mostrar outro numero da array e excluir o numero anterior da array
